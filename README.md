98ebeb9fa3c73c35330c9950dd48f882dacdad57
sjeandeaux <stephane.jeandeaux@gmail.com>
2014-11-15 16:07:18 +0100

    add configuration binding address

---
+++ README.md
@@ -0,0 +1,20 @@
+ElSimulator
+========
+
+##call with default
+
+###Command
+
+'''
+./elSimulator 
+''' 
+
+Start server on localhost:4000
+
+##call with flag
+
+'''
+./elSimulator -bindingAddress=localhost:9696
+''' 
+
+Start server on localhost:9696
--- main.go
+++ main.go
@@ -1,24 +1,45 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"net/http"
 )
+
+/**
+* Configuration Application
+ */
+type ElSimulatorConfig struct {
+	//url to call
+	bindingAddress string
+}
+
+//configuration to use
+var elSimulatorConfig = new(ElSimulatorConfig)
+
+//parse command in configuration
+func init() {
+	flag.StringVar(&elSimulatorConfig.bindingAddress, "bindingAddress", "localhost:4000", "The binding address")
+	flag.Parse()
+}
+
+/**
+* Bind address.
+ */
+func main() {
+	http.HandleFunc("/", ElSimulatorHandle)
+	http.ListenAndServe(elSimulatorConfig.bindingAddress, nil)
+}
 
 /**
 * Request <= /folder0/..../folderN?param1=param1&...paramN=valueN with POST or PUT content
 * Found file [folder configuration]/determine(request)
 * If the file does not exist then it sends a 404 else the file is written to the stream
  */
-func ElSimulator(
+func ElSimulatorHandle(
 	w http.ResponseWriter,
 	r *http.Request) {
 	//TODO get file
 	io.WriteString(w, "This life is a party!!!")
-}
 
-func main() {
-	http.HandleFunc("/", ElSimulator)
-	//TODO configuration binding
-	http.ListenAndServe("localhost:4000", nil)
 }
