06c08dc9e8ea1c8b766d22b35485ebbb6c4bf0d8
sjeandeaux <stephane.jeandeaux@gmail.com>
2014-11-17 22:15:28 +0100

    begin name file

--- elSimulator.go
+++ elSimulator.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/user"
 	"path/filepath"
+	"strings"
 )
 
 //Configuration Application
@@ -19,6 +22,13 @@ type ElSimulatorConfig struct {
 	//context web
 	context string
 }
+
+const (
+	separator        = "_"
+	separatorURL     = "/"
+	withoutParameter = "withoutParameter"
+	pathSeparator    = string(os.PathSeparator)
+)
 
 //configuration to use
 var elSimulatorConfig = new(ElSimulatorConfig)
@@ -83,12 +93,33 @@ func ElSimulatorHandle(
 
 // Find file if not found (or a other error) nil else file.
 func findFile(r *http.Request) *os.File {
-	//TODO generate name file
 	log.Println(elSimulatorConfig.baseDirectory, r.URL.Path, r.URL.Query(), r.Form)
-	file, err := os.Open("TODO.go") // For read access.
+	//TODO for on type file .txt, .xml, .json
+	fileToRead := elSimulatorConfig.baseDirectory + strings.Replace(r.URL.Path, separatorURL, pathSeparator, -1) + pathSeparator + nameFile(r.URL.Query())
+	log.Println(fileToRead)
+	file, err := os.Open(fileToRead) // For read access.
 	if err != nil {
 		log.Println(err)
 		return nil
 	}
 	return file
+}
+
+//query to generate name.
+//TODO sort
+//TODO filter
+//TODO body content.
+func nameFile(query url.Values) string {
+	var buffer bytes.Buffer
+
+	for key, value := range query {
+		buffer.WriteString(separator)
+		buffer.WriteString(key)
+		buffer.WriteString(separator)
+		buffer.WriteString(strings.Join(value, separator))
+	}
+	if buffer.Len() == 0 {
+		return withoutParameter
+	}
+	return buffer.String()[1:]
 }
