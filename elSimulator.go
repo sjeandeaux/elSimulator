5270b4bba4435f3ef3eee8fcbcf42b935fc6010d
sjeandeaux <stephane.jeandeaux@gmail.com>
2014-11-18 23:07:30 +0100

    add file in request with content-type

--- elSimulator.go
+++ elSimulator.go
@@ -3,14 +3,15 @@ package main
 import (
 	"bytes"
 	"flag"
-	"io"
 	"log"
+	"mime"
 	"net/http"
 	"net/url"
 	"os"
 	"os/user"
 	"path/filepath"
 	"strings"
+	"text/template"
 )
 
 //Configuration Application
@@ -19,8 +20,6 @@ type ElSimulatorConfig struct {
 	bindingAddress string
 	//directory with file to read
 	baseDirectory string
-	//context web
-	context string
 }
 
 const (
@@ -37,11 +36,10 @@ var elSimulatorConfig = new(ElSimulatorConfig)
 func init() {
 	const (
 		defaultBindingAddress = "localhost:4000"
-		defaultContext        = "/elSimulator/"
-		defaultBaseDirectory  = "" //elSimulatorCurrent if default is current directory
+		elSimulatorCurrent    = "elSimulatorCurrent"
+		defaultBaseDirectory  = elSimulatorCurrent //elSimulatorCurrent if default is current directory
 	)
 	flag.StringVar(&elSimulatorConfig.bindingAddress, "bindingAddress", defaultBindingAddress, "The binding address")
-	flag.StringVar(&elSimulatorConfig.context, "context", defaultContext, "The context")
 	flag.StringVar(&elSimulatorConfig.baseDirectory, "baseDirectory", defaultBaseDirectory, "directory with file to read (elSimulatorCurrent to use directory elSimulator)")
 	flag.Parse()
 	//use home'user
@@ -54,7 +52,7 @@ func init() {
 		elSimulatorConfig.baseDirectory = usr.HomeDir
 		break
 
-	case "elSimulatorCurrent":
+	case elSimulatorCurrent:
 		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 		if err != nil {
 			log.Fatal(err)
@@ -66,8 +64,9 @@ func init() {
 }
 
 //Bind address.
+//TODO administration status request in levelDb???
 func main() {
-	http.HandleFunc(elSimulatorConfig.context, ElSimulatorHandle)
+	http.HandleFunc("/file/", ElSimulatorHandle)
 	log.Println("start on ", elSimulatorConfig.bindingAddress)
 	err := http.ListenAndServe(elSimulatorConfig.bindingAddress, nil)
 	if err != nil {
@@ -78,6 +77,7 @@ func main() {
 // Request <= /folder0/..../folderN?param1=param1&...paramN=valueN with POST or PUT content
 // Found file [folder configuration]/determine(request)
 // If the file does not exist then it sends a 404 else the file is written to the stream
+//TODO add parameter in template
 func ElSimulatorHandle(
 	w http.ResponseWriter,
 	r *http.Request) {
@@ -86,32 +86,52 @@ func ElSimulatorHandle(
 		http.Error(w, "The life is a party!!!", http.StatusNotFound)
 	} else {
 		//TODO template
-		io.WriteString(w, "With file, it should be better")
+		name := f.Name()
+		t, err := template.ParseFiles(name)
+		if err != nil {
+			log.Printf("error template => %s", err)
+		}
+		w.Header().Add("content-type", mime.TypeByExtension(filepath.Ext(name)))
+		t.Execute(w, nil)
 	}
 
 }
 
 // Find file if not found (or a other error) nil else file.
+// Url => localhost/file/test/sub?param1=value1&param2=value2
+// File /.../file/test/sub/param1_value1_param2_value2
+// If not found first file matchs pattern /.../file/test/sub/param1_value1_param2_value2*
 func findFile(r *http.Request) *os.File {
-	log.Println(elSimulatorConfig.baseDirectory, r.URL.Path, r.URL.Query(), r.Form)
-	//TODO for on type file .txt, .xml, .json
 	fileToRead := elSimulatorConfig.baseDirectory + strings.Replace(r.URL.Path, separatorURL, pathSeparator, -1) + pathSeparator + nameFile(r.URL.Query())
-	log.Println(fileToRead)
-	file, err := os.Open(fileToRead) // For read access.
+	log.Printf("file => %s", fileToRead)
+	//file exists
+	file, err := os.Open(fileToRead)
+	if err == nil {
+		return file
+	}
+	log.Printf("error => %s", err)
+	allFile, errTwo := filepath.Glob(fileToRead + "*")
+	if errTwo != nil || len(allFile) == 0 {
+		log.Println(errTwo)
+		return nil
+	}
+
+	log.Printf("file => %s", allFile[0])
+	file, err = os.Open(allFile[0]) // For read access.
 	if err != nil {
-		log.Println(err)
 		return nil
 	}
 	return file
+
 }
 
 //query to generate name.
-//TODO sort
+//TODO sort parameter and valueS
 //TODO filter
 //TODO body content.
 func nameFile(query url.Values) string {
 	var buffer bytes.Buffer
-
+	//append all key value
 	for key, value := range query {
 		buffer.WriteString(separator)
 		buffer.WriteString(key)
@@ -121,5 +141,6 @@ func nameFile(query url.Values) string {
 	if buffer.Len() == 0 {
 		return withoutParameter
 	}
+	//remove first separator
 	return buffer.String()[1:]
 }
